4305e576438ddf8b710f677c2954434b0c0d4321
Gabor Szabo <gabor@szabgab.com>
2019-01-02 22:43:51 +0200

    placeholder for testing

---
+++ test_backup.py
@@ -0,0 +1,9 @@
+import backup
+
+# create a temporary directory for the source files
+# create a temp directory for the git repository
+# run the backup
+# check the results
+#backup.run()
+def test_backup():
+    assert True
