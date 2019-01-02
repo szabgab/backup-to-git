a4ba68c1b9ae55348c43f7b043a8b7b46e1effbe
Gabor Szabo <gabor@szabgab.com>
2019-01-02 22:43:09 +0200

    work on the backup script

--- backup.py
+++ backup.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
+import argparse
+import logging
+import json
 import os
-import subprocess
 import shutil
+import subprocess
 
 '''
 TODO:
@@ -13,30 +16,44 @@ Error handling
 Test the various cases.
 '''
 
-def backup():
-    git_dir = os.path.dirname(os.path.abspath(__file__))
-    dropbox_dir = '/home/gabor/Dropbox/notes'
+def run():
+    if not os.path.exists(args.config):
+        exit('"{}" does not exist'.format(args.config)) 
+
+    with open(args.config) as fh:
+        config = json.load(fh)
+    #print(config)
+
+    target_dir = config['target']
+    source_dir = config['source']
     
     git = 'git'
     
-    if not os.path.exists(dropbox_dir):
-        exit('Dropbox direcory "{dropbox_dir}" does not exist'.format(dropbox_dir = dropbox_dir))
+    if not os.path.exists(source_dir):
+        exit('Source directory "{source_dir}" does not exist'.format(source_dir = source_dir))
 
-    for thing in os.listdir(dropbox_dir):
-        path_to = os.path.join(dropbox_dir, thing)
+    for thing in os.listdir(source_dir):
+        logging.info(thing)
+        path_to = os.path.join(source_dir, thing)
         if os.path.isfile(path_to):
-            shutil.copy(path_to, git_dir)
+            logging.info('Copy file {} to {}'.format(path_to, target_dir))
+            shutil.copy(path_to, target_dir)
         elif os.path.isdir(path_to):
-            shutil.copytree(path_to, git_dir)
+            logging.info('Copy tree {} to {}'.format(path_to, target_dir))
+            shutil.copytree(path_to, target_dir)
         else:
             print('Not file and not directory {}'.format(thing))
-    os.chdir(git_dir)
+    os.chdir(target_dir)
     status = subprocess.check_output([git, 'status', '--porcelain'])
     #print(status)
-    if status:
-        add = subprocess.check_output([git, 'add', '.'])
-        commit = subprocess.check_output([git, 'commit', '-m', 'update'])
-        push = subprocess.check_output([git, 'push'])
+    #if status:
+    #    add = subprocess.check_output([git, 'add', '.'])
+    #    commit = subprocess.check_output([git, 'commit', '-m', 'update'])
+    #    push = subprocess.check_output([git, 'push'])
 
 if __name__ == '__main__':
-    backup()
+    logging.basicConfig(level = logging.INFO)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', help='Config file', required=True)
+    args = parser.parse_args()
+    run()
