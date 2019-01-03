7718125f121e7fa528875c4605c31945378d3a6e
Gabor Szabo <gabor@szabgab.com>
2019-01-03 17:57:57 +0200

    start writing tests

--- test_backup.py
+++ test_backup.py
@@ -1,3 +1,7 @@
+import json
+import os
+import sys
+
 import backup
 
 # create a temporary directory for the source files
@@ -5,5 +9,21 @@ import backup
 # run the backup
 # check the results
 #backup.run()
-def test_backup():
+def test_backup(tmpdir):
+    print(tmpdir)
+    source_dir = os.path.join(tmpdir, 'src')
+    target_dir = os.path.join(tmpdir, 'target')
+    config_file = os.path.join(tmpdir, 'cfg.json')
+    os.mkdir(source_dir)
+    os.mkdir(target_dir)
+
+    with open(config_file, 'w') as fh:
+       json.dump({
+	   'source': source_dir,
+	   'target': target_dir,
+       }, fh)
+
+    print(sys.argv)
+    sys.argv = ['backup', '--config', config_file]
+    backup.main()
     assert True
